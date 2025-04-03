da99f53d0c4d716fda9376dd1b1a72c409f01bfd
Jamal Majadle <jamal.majadle02@students.kinneret.ac.il>
2025-04-03 13:44:13 +0300

    reveal api with accountId and secretId

---
+++ src/libs/database/account/reveal-secret-by-Ids.ts
@@ -0,0 +1,28 @@
+import { db } from '@libs/database/db';
+import { decrypt } from 'encrypt-tools';
+import { env } from '@libs/env';
+import { NotFoundError } from '@events-project/common';
+
+export const revealSecretByIds = async (
+  accountId: string,
+  secretId: string
+): Promise<{ apiKey: string }> => {
+  const secretRecord = await db.secret.findFirst({
+    where: {
+      id: secretId,
+      accountId: accountId,
+      isActive: true,
+    },
+  });
+
+  if (!secretRecord) throw new NotFoundError('SECRET_NOT_FOUND');
+
+  const [iv, ciphertext] = secretRecord.secret.split(':');
+  const apiKey = decrypt({
+    secretKey: env('SECRET_ENCRYPT_KEY'),
+    iv,
+    ciphertext,
+  });
+
+  return { apiKey };
+};
