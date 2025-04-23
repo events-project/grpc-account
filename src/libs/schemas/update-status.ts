581c3b41314e8bb9a589eaa161d84fc74fb8f446
Roma Fominih <roma811@icloud.com>
2025-04-23 15:22:26 +0300

    Update protoc version and add payment status transformation logic

--- src/libs/schemas/update-status.ts
+++ src/libs/schemas/update-status.ts
@@ -1,10 +1,23 @@
-import { z } from 'zod';
+import { PaymentStatus as GrpcPaymentStatus } from '@grpc/service';
 import { PaymentStatus } from '@prisma/client';
+import { z } from 'zod';
+// import { PaymentStatus } from '@prisma/client';
 
 export const UpdateBillingSchema = z.object({
   id: z.string().uuid(),
   paymentId: z.string().min(1),
-  paymentStatus: z.nativeEnum(PaymentStatus),
+  paymentStatus: z.nativeEnum(GrpcPaymentStatus).transform((value): PaymentStatus => {
+    switch (value) {
+      case GrpcPaymentStatus.PENDING:
+        return 'PENDING';
+      case GrpcPaymentStatus.PAID:
+        return 'PAID';
+      case GrpcPaymentStatus.FAILED:
+        return 'FAILED';
+      default:
+        throw new Error('Invalid payment status');
+    }
+  }),
 });
 
 export type UpdateBillingParams = z.infer<typeof UpdateBillingSchema>;
